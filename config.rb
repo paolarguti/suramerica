a0ae10004dcdb514a02c869edc12163d49fb6f76
Adolfo Builes <builes.adolfo@gmail.com>
2015-10-21 08:27:39 -0500

    Update

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/suramerica/"
-http_images_path = "/suramerica/images"
+http_path = "/"
+http_images_path = "/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/suramerica/fonts"
-css_dir = "public/suramerica/stylesheets"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
